c1fa6845ad1d84598a694b893826f07074bdcf2a
Sam Reh <samuelreh@gmail.com>
2017-11-10 08:03:00 -0800

    Persist to db, fin, index

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-import random, os
-from flask import Flask, render_template, redirect, url_for, send_from_directory
-app = Flask(__name__)
+import random, os, datetime
+from random import *
+from flask import Flask, render_template, redirect, url_for, send_from_directory, request, session
+import pyrebase
 
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+
+config = {
+  "apiKey": "AIzaSyCyMYbU4swN-cSYyV7KefYyLuEf0G2bsMg",
+  "authDomain": "",
+  "databaseURL": "https://bahdah-1.firebaseio.com",
+  "storageBucket":"",
+  "serviceAccount": "./bahdah-1-firebase-adminsdk-naxmt-0eae5a615f.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
 
 def envd(path):
     if 'UP_STAGE' in os.environ:
@@ -12,24 +26,51 @@ def envd(path):
 
 @app.route("/", methods=['GET'])
 def get_index():
-    return redirect(envd(url_for('get_run_clip', run_id=1, clip_id=1)))
+    trial_url = envd(url_for('create_trial'))
+    return render_template('index.html', trial_url=trial_url)
 
 
-@app.route("/clips/<clip_filename>", methods=['GET'])
-def get_clip(clip_filename):
-    return send_from_directory('clips', clip_filename)
+@app.route("/trials", methods=['POST'])
+def create_trial():
+    files = os.listdir('clips') * 3
+    shuffle(files)
+    session['files'] = files
+    session['name'] = request.form['name']
+    return redirect(envd(url_for('get_event', event_id=0)))
 
 
-@app.route("/runs/<run_id>/clips/<clip_id>", methods=['GET'])
-def get_run_clip(run_id, clip_id):
-    clip_filename = str(random.randrange(1, 3)) + '.mp3'
-    clip_url = envd(url_for('get_clip', clip_filename=clip_filename))
-    bah_result_url = envd(url_for('post_run_clip_result', run_id=run_id, clip_id=clip_id, result='bah'))
-    dah_result_url = envd(url_for('post_run_clip_result', run_id=run_id, clip_id=clip_id, result='dah'))
-    return render_template('index.html', bah_result_url=bah_result_url, dah_result_url=dah_result_url, clip_url=clip_url, clip_id=clip_id)
+@app.route("/files/<filename>", methods=['GET'])
+def get_file(filename):
+    return send_from_directory('clips', filename)
 
 
-@app.route("/runs/<run_id>/clips/<clip_id>/<result>", methods=['POST'])
-def post_run_clip_result(run_id, clip_id, result):
-    next_clip_id = int(clip_id) + 1
-    return redirect(envd(url_for('get_run_clip', run_id=run_id, clip_id=next_clip_id)))
+@app.route("/events/<event_id>", methods=['GET'])
+def get_event(event_id):
+    if int(event_id) >= len(session['files']):
+        return redirect(envd(url_for('get_fin')))
+    filename = session['files'][int(event_id)]
+    file_url = envd(url_for('get_file', filename=filename))
+    result_url = envd(url_for('create_result'))
+    total = (len(session['files']) - 1)
+    return render_template('event.html', result_url=result_url, event_id=event_id, file_url=file_url, total=total, filename=filename)
+
+@app.route("/fin", methods=['GET'])
+def get_fin():
+    return render_template('fin.html')
+
+
+@app.route("/results", methods=['POST'])
+def create_result():
+    name = session["name"]
+    event_id = request.form["event_id"]
+    data = request.form.to_dict()
+    data["ip"] = get_ip()
+    data["time"] = datetime.datetime.now().isoformat()
+    db.child("subjects").child(name).child("results").child(event_id).set(data)
+    return redirect(envd(url_for('get_event', event_id=int(event_id) + 1)))
+
+def get_ip():
+    if request.headers.getlist("X-Forwarded-For"):
+       return request.headers.getlist("X-Forwarded-For")[0]
+    else:
+       return request.remote_addr
